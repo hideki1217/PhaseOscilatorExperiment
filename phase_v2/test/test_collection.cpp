37bb4e523261136e63d4a124f0ca1a7737656b09
h1de <okumurahidekikangeki@gmail.com>
2023-10-18 15:49:15 +0000

    Added FixedQueue

---
+++ phase_v2/test/test_collection.cpp
@@ -0,0 +1,20 @@
+#include <cassert>
+#include <collection.hpp>
+#include <vector>
+
+void test_FixedQueue {
+  auto q = lib::collection::FixedQueue<int>(3, 0);
+  assert(q.sum() == 0);
+  assert(q.size() == 3);
+
+  assert(q.push(1) == 0);
+  assert(q.sum() == 1);
+  assert(q.push(2) == 0);
+  assert(q.sum() == 3);
+  assert(q.push(3) == 0);
+  assert(q.sum() == 6);
+  assert(q.push(4) == 1);
+  assert(q.sum() == 9);
+}
+
+int main() { test_FixedQueue(); }

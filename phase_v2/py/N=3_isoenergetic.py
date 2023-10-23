74fd7238a72023d5b4c519c67c0ea16dde921226
HidekiOkumura <okumurahidekikangeki@gmail.com>
2023-10-23 11:53:27 +0900

    :sparkles: draw a heatmap showing N=3 isoenergetic

---
+++ phase_v2/py/N=3_isoenergetic.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+import opypy
+
+
+def main():
+    file = Path(__file__)
+    data = file.parent / "output" / file.stem
+    if not data.exists():
+        data.mkdir()
+
+    ndim = 3
+    w = np.array([-1, 0, 1.])
+    model = opypy.OrderEvaluator(
+        window=30000, epsilon=1e-4, dt=0.01, max_iteration=100000, ndim=ndim)
+
+    def f(K1, K2, K3, w):
+        K_ = np.array([0, K1, K2,
+                       K1, 0, K3,
+                       K2, K3, 0])
+
+        status = model.eval(K_, w)
+
+        print(f"{K1}, {K2}, {K3}")
+        if (status.value == 0):
+            return model.result()
+        else:
+            return np.nan
+
+    def make_isoenergetic(E):
+        E *= ndim / 2
+        def iso(p): return f(p[0] * E, p[1] * E, p[2] * E, w=w)
+        return iso
+
+    import ternary
+
+    def draw(E):
+        figure, tax = ternary.figure(scale=30)
+        ternary.heatmapf(make_isoenergetic(E), boundary=True,
+                         style="triangular", vmin=0, vmax=1, scale=100)
+        tax.boundary(linewidth=2.0)
+        plt.box(False)
+        plt.axis("off")
+        plt.tight_layout()
+        plt.savefig(data / f"E={E:.2f}.png")
+        plt.close()
+
+    for E in map(lambda x: x*0.05, range(61)):
+        draw(E)
+
+
+if __name__ == "__main__":
+    main()

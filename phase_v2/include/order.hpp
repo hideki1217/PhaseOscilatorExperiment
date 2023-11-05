609a0b6bd3700a8c8a8641afc5a2374d17e6c162
h1de <okumurahidekikangeki@gmail.com>
2023-11-05 12:12:13 +0000

    make Inner class to private

--- phase_v2/include/order.hpp
+++ phase_v2/include/order.hpp
@@ -3,44 +3,40 @@
 
 namespace lib {
 namespace order {
-
-template <typename Real>
-struct InnerUnit {
-  Real cos;
-  Real sin;
-
-  InnerUnit(Real cos, Real sin) : cos(cos), sin(sin) {}
-};
-
 template <typename Real>
 class KuramotoFixed {
+  struct Unit {
+    Real cos;
+    Real sin;
+
+    Unit(Real cos, Real sin) : cos(cos), sin(sin) {}
+  };
+
  public:
   const int ndim;
   KuramotoFixed(int window, int ndim)
       : ndim(ndim), cos_q(window, 0.), sin_q(window, 0.) {}
 
-  InnerUnit<Real> push(const Real *s) {
+  Unit push(const Real *s) {
     Real cos_mean = 0;
     for (int i = 0; i < ndim; i++) {
       cos_mean += std::cos(s[i]);
     }
     cos_mean /= ndim;
-    auto cos_pop = cos_q.push(cos_mean);
 
     Real sin_mean = 0;
     for (int i = 0; i < ndim; i++) {
       sin_mean += std::sin(s[i]);
     }
     sin_mean /= ndim;
-    auto sin_pop = sin_q.push(sin_mean);
 
-    return InnerUnit(cos_pop, sin_pop);
+    return push({cos_mean, sin_mean});
   }
 
-  InnerUnit<Real> push(const InnerUnit<Real> inner) {
+  Unit push(const Unit inner) {
     auto cos_pop = cos_q.push(inner.cos);
     auto sin_pop = sin_q.push(inner.sin);
-    return InnerUnit(cos_pop, sin_pop);
+    return Unit(cos_pop, sin_pop);
   }
 
   Real value() {

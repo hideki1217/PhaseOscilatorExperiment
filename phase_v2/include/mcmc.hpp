8fdfd53b1550c1f4090a2e6664e6fa9ddc53711e
h1de <okumurahidekikangeki@gmail.com>
2023-11-13 05:56:53 +0000

    impl sample mcmc

--- phase_v2/include/mcmc.hpp
+++ phase_v2/include/mcmc.hpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cmath>
+#include <iostream>
 #include <memory>
 #include <opy.hpp>
 #include <random>
@@ -23,18 +24,51 @@ class UniformSelector {
     return items[dist(rng)];
   }
 };
+
+class Statistics {
+ public:
+  Statistics(int num_class) : counts(num_class, 0) {}
+
+  void push(int x) {
+    assert(x < counts.size());
+    counts[x]++;
+    total++;
+  }
+
+  double rate(int x) {
+    assert(x < counts.size());
+    return double(counts[x]) / total;
+  }
+
+  void reset() {
+    total = 0;
+    std::fill(counts.begin(), counts.end(), 0);
+  }
+
+ private:
+  uint total = 0;
+  std::vector<uint> counts;
+};
 }  // namespace utils
+
+namespace mcmc {
+enum Result {
+  Accepted = 0,
+  Rejected,
+  MinusConnection,
+  SmallOrder,
+  NotConverged,
+  LENGTH
+};
+const char *result2str(Result result) {
+  static const char *nameTT[] = {"Accepted", "Rejected", "MinusConnection",
+                                 "SmallOrder", "NotConverged"};
+  return nameTT[result];
+}
 
 template <typename Order>
 class BolzmanMarkovChain {
   using Real = typename Order::V;
-  enum Result {
-    Accepted = 0,
-    Rejected,
-    MinusConnection,
-    UnderLimit,
-    NotConverged,
-  };
 
  public:
   const int ndim;
@@ -65,14 +99,22 @@ class BolzmanMarkovChain {
     for (int i = 0; i < ndim * ndim; i++) K_sum += K[i];
   }
 
-  Real *connection() { return &K[0]; }
-  Real energy() { return K_sum / ndim; }
+  const Real *connection() const { return &K[0]; }
+  Real energy() const { return K_sum / ndim; }
 
-  void step(int n) {
-    for (int i = 0; i < n; i++) step();
+  /**
+   * reprica exchange
+   */
+  bool try_swap(BolzmanMarkovChain<Order> &rhs) {
+    const Real rate = std::exp((beta - rhs.beta) * (energy() - rhs.energy()));
+    if (1 <= rate || std::uniform_real_distribution<>(0, 1)(_rng) < rate) {
+      K.swap(rhs.K);
+      return true;
+    }
+    return false;
   }
 
-  void step() {
+  Result step() {
     const auto idx = _target_indexs.select(_rng);
     const auto xdi = (idx % ndim) * ndim + (idx / ndim);
     const auto prev_K_idx = K[idx];
@@ -89,6 +131,8 @@ class BolzmanMarkovChain {
       K[idx] = K[xdi] = prev_K_idx;
       K_sum = prev_K_sum;
     }
+
+    return result;
   }
 
  private:
@@ -103,7 +147,7 @@ class BolzmanMarkovChain {
     }
     const auto R = evaluator.result();
     if (R < threshold) {
-      return Result::UnderLimit;
+      return Result::SmallOrder;
     }
 
     // dE = E(t+1) - E(t)
@@ -126,4 +170,6 @@ class BolzmanMarkovChain {
   utils::UniformSelector<int> _target_indexs;
   std::mt19937 _rng;
 };
+}  // namespace mcmc
+
 }  // namespace lib
---
+++ phase_v2/src/sample_mcmc.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <mcmc.hpp>
+#include <order.hpp>
+#include <random>
+
+using namespace lib;
+
+template <typename Order>
+struct Param {
+  using target_t = Order;
+
+  const int ndim;
+  const double* w;
+  const double* K;
+  const double threshold;
+
+  const int num_reprica;
+  const double* betas;
+  const double* scales;
+
+  const int burn_in;
+  const int T;
+  const int swap;
+
+  const int seed = 35;
+};
+
+struct Result {
+  std::vector<double> exchange_rates;
+};
+
+template <typename target_t>
+Result reprica_exchange(const Param<target_t> p) {
+  std::mt19937 rng(p.seed);
+
+  std::vector<int> count_total(p.num_reprica - 1, 0);
+  std::vector<int> count_accepted(p.num_reprica - 1, 0);
+
+  std::vector<mcmc::BolzmanMarkovChain<target_t>> markovs;
+  for (int r = 0; r < p.num_reprica; r++) {
+    markovs.emplace_back(mcmc::BolzmanMarkovChain<target_t>(
+        p.ndim, p.w, p.K, p.threshold, p.betas[r], p.scales[r], rng()));
+  }
+
+// Burn-in
+#pragma omp parallel for
+  for (int r = 0; r < p.num_reprica; r++) {
+    for (int i = 0; i < p.burn_in; i++) markovs[r].step();
+  }
+
+  for (int e = 0; e < 100; e++) {
+#pragma omp parallel for
+    for (int r = 0; r < p.num_reprica; r++) {
+      for (int i = 0; i < p.T * p.swap; i++) markovs[r].step();
+    }
+
+    for (int i = e % 2; i < p.num_reprica - 1; i += 2) {
+      count_total[i]++;
+      if (markovs[i].try_swap(markovs[i + 1])) {
+        count_accepted[i]++;
+      }
+    }
+  }
+
+  std::vector<double> exchange_rates(p.num_reprica);
+  for (int r = 0; r < p.num_reprica; r++) {
+    exchange_rates[r] = double(count_accepted[r]) / count_total[r];
+  }
+  return {exchange_rates};
+}
+
+int main() {
+  using target_t = order::Kuramoto<double>;
+  const int ndim = 2;
+  const double w[] = {-1, 1};
+  const double K[] = {0, 5, 5, 0};
+  const double threshold = 0.78;
+
+  const double min_beta = 1.0;
+  const double max_beta = 100;
+  const double target_accept_rate = 0.5;
+  const double target_exchange_rate = 0.5;
+  const int burn_in = 100;
+
+  double scale;
+  {
+    std::cout << "==== Measure scale ==== : target_accept_rate = "
+              << target_accept_rate << std::endl;
+
+    double scale_l = 1e-10;
+    double scale_r = 4.0;
+    for (int i = 0; i < 10; i++) {
+      scale = (scale_r + scale_l) / 2;
+
+      auto stat = utils::Statistics(mcmc::Result::LENGTH);
+      auto markov = mcmc::BolzmanMarkovChain<target_t>(ndim, w, K, threshold,
+                                                       min_beta, scale, 42);
+      for (int i = 0; i < burn_in; i++) markov.step();  // Burn-In
+
+      for (int i = 0; i < 1000; i++) stat.push(markov.step());
+
+      const auto accept_rate = stat.rate(mcmc::Result::Accepted);
+      if (target_accept_rate < accept_rate) {
+        scale_l = scale;
+      } else {
+        scale_r = scale;
+      }
+    }
+
+    std::cout << "beta = " << min_beta << ", scale = " << scale << std::endl;
+  }
+
+  double beta = min_beta;
+  Result result;
+  std::vector<double> betas({min_beta}), scales({scale});
+  {
+    std::cout << "==== Measure Beta series ==== : target_exchange_rate = "
+              << target_exchange_rate << std::endl;
+
+    int index = 0;
+    std::cout << index + 1 << ": " << betas[index] << std::endl;
+    while (beta < max_beta) {
+      // 高温から順に、二分探索で交換率 target_exchange_rate を目指す
+      // 決まったら、それをbetasにpushして max_beta に到達するまで続ける
+      double beta_l = betas[betas.size() - 1];
+      double beta_r = max_beta * 2;
+      for (int i = 0; i < 10; i++) {
+        beta = (beta_r + beta_l) / 2;
+
+        betas.push_back(beta);
+        scales.push_back(min_beta / beta * scale);
+
+        // betasでレプリカ交換法
+        Param<target_t> param = {ndim,
+                                 w,
+                                 K,
+                                 threshold,
+                                 static_cast<int>(betas.size()),
+                                 &betas[0],
+                                 &scales[0],
+                                 burn_in,
+                                 10,
+                                 10};
+        result = reprica_exchange(param);
+
+        // 最後の二つの 交換成立回数/交換試行回数 を計測し、結果を見る
+        const double exchange_rate = result.exchange_rates[betas.size() - 2];
+        if (exchange_rate < target_exchange_rate) {
+          beta_r = beta;
+        } else {
+          beta_l = beta;
+        }
+
+        betas.pop_back();
+        scales.pop_back();
+      }
+
+      betas.push_back(beta);
+      scales.push_back(min_beta / beta * scale);
+      index++;
+      std::cout << index + 1 << ": " << betas[index] << std::endl;
+      for (int i = 0; i < betas.size() - 1; i++) {
+        std::cout << i << "<->" << i + 1 << " = " << result.exchange_rates[i]
+                  << std::endl;
+      }
+    }
+  }
+
+  std::cout << "==== Total result ====" << std::endl;
+  std::cout << "const auto base_scale = " << scale << ";" << std::endl;
+  std::cout << "const auto betas[] = {";
+  for (auto beta : betas) {
+    std::cout << beta << ",";
+  }
+  std::cout << "};" << std::endl;
+  std::cout << "const auto scales[] = {";
+  for (auto scale : scales) {
+    std::cout << scale << ",";
+  }
+  std::cout << "};" << std::endl;
+}

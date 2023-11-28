1d8910813e2c00fbbb60c0ee657ebb531b835e81
h1de <okumurahidekikangeki@gmail.com>
2023-11-28 14:52:44 +0000

    use RepricaMCMC in sample_mcmc and refactor

--- phase_v2/src/sample_mcmc.cpp
+++ phase_v2/src/sample_mcmc.cpp
@@ -40,33 +40,21 @@ Result reprica_exchange(const Param<target_t> p) {
   std::vector<int> count_total(p.num_reprica - 1, 0);
   std::vector<int> count_accepted(p.num_reprica - 1, 0);
 
-  std::vector<mcmc::BolzmanMarkovChain<target_t>> markovs;
-  for (int r = 0; r < p.num_reprica; r++) {
-    markovs.emplace_back(mcmc::BolzmanMarkovChain<target_t>(
-        p.ndim, p.w, p.K, p.threshold, p.betas[r], p.scales[r], rng()));
-  }
+  mcmc::RepricaMCMC<target_t> mcmc_list(
+      p.ndim, p.w, p.K, p.threshold, p.num_reprica, p.betas, p.scales, rng());
 
   // Burn-in
-  for (int r = 0; r < p.num_reprica; r++) {
-    thread_pool.post([&markovs, &p, r]() {
-      for (int i = 0; i < p.burn_in; i++) markovs[r].step();
-    });
-  }
-  thread_pool.join();
+  mcmc_list.step(thread_pool, p.burn_in);
 
   for (int e = 0; e < 1000; e++) {
-    for (int r = 0; r < p.num_reprica; r++) {
-      thread_pool.post([&markovs, &p, r]() {
-        for (int i = 0; i < p.T * p.swap; i++) markovs[r].step();
-      });
-    }
-    thread_pool.join();
+    mcmc_list.step(thread_pool, p.T * p.swap);
 
     eprintf("%d's try_swap", e + 1);
-    for (int i = e % 2; i < p.num_reprica - 1; i += 2) {
-      count_total[i]++;
-      if (markovs[i].try_swap(markovs[i + 1])) {
-        count_accepted[i]++;
+    const auto exchange_res = mcmc_list.exchange();
+    for (int r = 0; r < p.num_reprica - 1; r++) {
+      if ((exchange_res.target >> r) & 1) {
+        count_total[r]++;
+        count_accepted[r] += (exchange_res.occured >> r) & 1;
       }
     }
   }
@@ -90,6 +78,7 @@ int main() {
   const double target_accept_rate = 0.5;
   const double target_exchange_rate = 0.5;
   const int burn_in = 100;
+  const double bisec_eps = 1e-2;
 
   double scale;
   {
@@ -98,17 +87,18 @@ int main() {
 
     double scale_l = 1e-10;
     double scale_r = 4.0;
-    for (int i = 0; i < 10; i++) {
+    const int bisec_iteration =
+        int(std::ceil(std::log2((scale_r - scale_l) / bisec_eps)));
+    for (int i = 0; i < bisec_iteration; i++) {
       scale = (scale_r + scale_l) / 2;
 
       auto stat = utils::Statistics(mcmc::Result::LENGTH);
       auto markov = mcmc::BolzmanMarkovChain<target_t>(ndim, w, K, threshold,
                                                        min_beta, scale, 42);
       for (int i = 0; i < burn_in; i++) markov.step();  // Burn-In
-
       for (int i = 0; i < 1000; i++) stat.push(markov.step());
-
       const auto accept_rate = stat.rate(mcmc::Result::Accepted);
+
       if (target_accept_rate < accept_rate) {
         scale_l = scale;
       } else {
@@ -119,26 +109,27 @@ int main() {
     std::cout << "beta = " << min_beta << ", scale = " << scale << std::endl;
   }
 
-  double beta = min_beta;
-  Result result;
   std::vector<double> betas({min_beta}), scales({scale});
   {
     std::cout << "==== Measure Beta series ==== : target_exchange_rate = "
               << target_exchange_rate << std::endl;
+    std::cout << betas.size() << ": " << betas.back() << std::endl;
 
-    int index = 0;
-    std::cout << index + 1 << ": " << betas[index] << std::endl;
-    while (beta < max_beta) {
+    while (betas.back() < max_beta) {
+      Result result;
+      double beta;
+
       // 高温から順に、二分探索で交換率 target_exchange_rate を目指す
       // 決まったら、それをbetasにpushして max_beta に到達するまで続ける
-      double beta_l = betas[betas.size() - 1];
+      double beta_l = betas.back();
       double beta_r = max_beta * 2;
-      for (int i = 0; i < 10; i++) {
+      const int bisec_iteration =
+          int(std::ceil(std::log2((beta_r - beta_l) / bisec_eps)));
+      for (int i = 0; i < bisec_iteration; i++) {
         beta = (beta_r + beta_l) / 2;
 
         betas.push_back(beta);
         scales.push_back(min_beta / beta * scale);
-
         // betasでレプリカ交換法
         Param<target_t> param = {ndim,
                                  w,
@@ -151,23 +142,22 @@ int main() {
                                  10,
                                  10};
         result = reprica_exchange(param);
-
+        betas.pop_back();
+        scales.pop_back();
         // 最後の二つの 交換成立回数/交換試行回数 を計測し、結果を見る
-        const double exchange_rate = result.exchange_rates[betas.size() - 2];
+        const double exchange_rate = result.exchange_rates.back();
+
         if (exchange_rate < target_exchange_rate) {
           beta_r = beta;
         } else {
           beta_l = beta;
         }
-
-        betas.pop_back();
-        scales.pop_back();
       }
 
       betas.push_back(beta);
       scales.push_back(min_beta / beta * scale);
-      index++;
-      std::cout << index + 1 << ": " << betas[index] << std::endl;
+
+      std::cout << betas.size() << ": " << betas.back() << std::endl;
       for (int i = 0; i < betas.size() - 1; i++) {
         std::cout << i << "<->" << i + 1 << " = " << result.exchange_rates[i]
                   << std::endl;

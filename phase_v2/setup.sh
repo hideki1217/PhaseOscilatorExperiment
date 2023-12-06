3ba9be4cfc8637f609ec68beae4279aafa1832e2
h1de <okumurahidekikangeki@gmail.com>
2023-12-06 02:03:50 +0000

    add dependency to json and boost

--- phase_v2/setup.sh
+++ phase_v2/setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 git clone https://github.com/pybind/pybind11.git
+git clone https://github.com/nlohmann/json.git
+
+wget https://boostorg.jfrog.io/artifactory/main/release/1.83.0/source/boost_1_83_0.tar.gz
+tar -zxvf boost_1_83_0.tar.gz
+rm boost_1_83_0.tar.gz
+
 python3 -m venv venv
 source ./venv/bin/activate
 pip install -r requirements.txt

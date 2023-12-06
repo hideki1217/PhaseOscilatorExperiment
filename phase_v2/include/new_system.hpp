12ed129e01aba5d9763ad6ea0a30f69314a6d8a1
h1de <okumurahidekikangeki@gmail.com>
2023-12-06 07:08:56 +0000

    avxnize

--- phase_v2/include/new_system.hpp
+++ phase_v2/include/new_system.hpp
@@ -3,6 +3,11 @@
 #include <array>
 #include <cassert>
 
+#ifdef SIM_AVX2
+#include <ia32intrin.h>
+#include <immintrin.h>
+#endif
+
 namespace new_lib {
 namespace system {
 
@@ -24,12 +29,12 @@ struct System {
     assert(K[0 * Kstride + 1] == K[1 * Kstride + 0]);
   }
   void operator()(const state_t& x, state_t& dx, double t) {
-    system_impl(ndim, K, Kstride, w, t, &x[0], &dx[0]);
+    system_impl(K, Kstride, w, t, &x[0], &dx[0]);
   }
 
  private:
   template <typename real_t>
-  void system_impl(int ndim, const real_t* K, int Kstride, const real_t* w,
+  void system_impl(const real_t* K, int Kstride, const real_t* w,
                    const real_t t, const real_t* s, real_t* ds_dt) noexcept {
     for (int i = 0; i < ndim; i++) {
       ds_dt[i] = w[i];
@@ -41,6 +46,47 @@ struct System {
       }
     }
   }
+
+#ifdef SIM_AVX2
+
+  template <>
+  void system_impl(const double* K, int Kstride, const double* w,
+                   const double t, const double* s, double* ds_dt) noexcept {
+    constexpr int simd_size = 4;
+
+    for (int i = 0; i < ndim; i += simd_size) {
+      __m256d ds_dt_i = _mm256_load_pd(&w[i]);
+      __m256d s_i = _mm256_load_pd(&s[i]);
+      for (int j = 0; j < ndim; j++) {
+        __m256d sin_diff =
+            _mm256_sin_pd(_mm256_sub_pd(_mm256_set1_pd(s[j]), s_i));
+        __m256d K_ = _mm256_load_pd(&K[j * Kstride + i]);
+        ds_dt_i = _mm256_add_pd(ds_dt_i, _mm256_mul_pd(K_, sin_diff));
+      }
+
+      switch (ndim - i) {
+        case 3: {
+          __m256i mask = _mm256_setr_epi64x(~0, ~0, ~0, 0);
+          _mm256_maskstore_pd(&ds_dt[i], mask, ds_dt_i);
+          break;
+        }
+        case 2: {
+          __m256i mask = _mm256_setr_epi64x(~0, ~0, 0, 0);
+          _mm256_maskstore_pd(&ds_dt[i], mask, ds_dt_i);
+          break;
+        }
+        case 1: {
+          __m256i mask = _mm256_setr_epi64x(~0, 0, 0, 0);
+          _mm256_maskstore_pd(&ds_dt[i], mask, ds_dt_i);
+          break;
+        }
+        default:
+          _mm256_store_pd(&ds_dt[i], ds_dt_i);
+          break;
+      }
+    }
+  }
+#endif
 };
 }  // namespace system
 }  // namespace new_lib
